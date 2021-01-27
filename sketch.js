723f8474f8a1a6eb23c70189385b8cac2b65bd96
Yash3730 <73027606+Yash3730@users.noreply.github.com>
2021-01-27 19:46:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var gameState=0;
+var database;
+var playerCount=0;
+var form , player , game;
+var allPlayers;
+var cars , car1,car2,car3,car4;
+function setup(){
+    database = firebase.database();
+    createCanvas(displayWidth-20, displayHeight-30);
+   game = new Game();
+   game.getState();
+   game.start();
+
+}
+
+function draw(){
+    background("white");
+    if (playerCount===4){
+        game.update(1)
+    }
+   if (gameState===1){
+       clear()
+       game.play()
+   }
+}
+
